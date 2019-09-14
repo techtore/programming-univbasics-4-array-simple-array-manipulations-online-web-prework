e2bee86a124823227e219d7a20842c7c24a8e29a
Victoria Hale <vic.hale74@gmail.com>
2019-09-14 22:04:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,10 +33,9 @@ def using_push (array, color)
       end
       
       def using_concat(arr1, arr2)
-        my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-        more_favs = ["sports cars", "flatiron school"]
-        all_my_favs = my_favorite_things.concat(more_favs)
-        p all_my_favs
+        
+        new_array = arr1.concat(arr2)
+        p new_array
       end
       
   def using_insert(array, new_ele)
